1572a975889563627437933f4d77ba4f8cc3737b
harshal45521 <harshalsojitra10039@gmail.com>
2026-07-12 11:36:57 +0530

    feat: implement driver management routes and controller functions

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes'); // 1. Import routes
-
+const vehicleRoutes = require('./routes/vehicleRoutes');
+const driverRoutes = require('./routes/driverRoutes')
 dotenv.config();
 connectDB();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 
 // 2. Mount the routes
 app.use('/api/auth', authRoutes);
+app.use('/api/drivers', driverRoutes);
+app.use('/api/vehicles', vehicleRoutes);
 
 app.get('/', (req, res) => {
   res.send('TransitOps API is running...');
